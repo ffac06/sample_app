542d22f9c7a4c262b75582c855cb16af25a04a05
ffac06 <varnerina@mssm.org>
2010-06-30 11:39:32 -0400

    made title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    base = "Tutorial"
+    if @title.nil?
+      base
+    else
+      "#{base} | #{@title}"
+    end
+  end
 end
